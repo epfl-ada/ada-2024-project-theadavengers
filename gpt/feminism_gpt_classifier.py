19b426b4e92a6174f0cec3492e86efafdd570304
shrinidhivelan <shrinidhi.velan@epfl.ch>
2024-12-20 10:34:12 +0100

    shri partie on it

--- gpt/feminism_gpt_classifier.py
+++ gpt/feminism_gpt_classifier.py
@@ -6,6 +6,22 @@ from transformers import AdamW
 from tqdm import tqdm
 import torch.nn as nn
 
+
+class GPTClassifier(nn.Module):
+        def __init__(self, gpt_model, num_classes):
+            super(GPTClassifier, self).__init__()
+            self.gpt = gpt_model
+            self.dropout = nn.Dropout(0.1)
+            self.classifier = nn.Linear(self.gpt.config.hidden_size, num_classes)
+
+        def forward(self, input_ids, attention_mask):
+            outputs = self.gpt(input_ids=input_ids, attention_mask=attention_mask)
+            last_hidden_state = outputs.last_hidden_state
+            pooled_output = last_hidden_state[:, -1, :]
+            pooled_output = self.dropout(pooled_output)
+            logits = self.classifier(pooled_output)
+            return logits
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
